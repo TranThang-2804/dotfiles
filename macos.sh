5973994c0db5e7f2c7878e3e2f8ff51b277f8574
Tran Thang <tranthang@Trans-MacBook-Pro-3.local>
2023-07-11 13:58:31 +0700

    update

---
+++ macos.sh
@@ -0,0 +1,9 @@
+# This script is for setting up environment for mac
+
+# Setup vim
+rm -rf ~/.vimrc
+cp ./vim_configuration/.vimrc ~/.vimrc
+
+# Setup .zshrc
+rm -rf ~/.zshrc
+cp ./zsh_configuration/.zshrc ~/.zshrc
---
+++ vim_configuration/.vimrc
@@ -0,0 +1,6 @@
+nmap <CR> O<Esc>j
+nmap j jzz
+nmap k kzz
+nmap <BS> kdd
+nmap J gT
+nmap K gt
