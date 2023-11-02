4d39c1cf3269168c080045d5073df1b10e42bad3
Tran Thang <tranthang.dev@gmail.com>
2023-11-02 18:20:20 +0700

    update

--- LazyVim/lua/config/keymaps.lua
+++ LazyVim/lua/config/keymaps.lua
@@ -1,25 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
-vim.keymap.set("n", "j", "jzz", { remap = true, desc = "Go down and center" })
-vim.keymap.set("n", "k", "kzz", { remap = true, desc = "Go up and center" })
+vim.keymap.set("n", "j", "jzz", { desc = "Go down and center" })
+vim.keymap.set("n", "k", "kzz", { desc = "Go up and center" })
 
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { remap = true, desc = "Go up and center" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { remap = true, desc = "Go down and center" })
-vim.keymap.set("n", "<C-y>", "<C-y>zz", { remap = true, desc = "Go down and center" })
-vim.keymap.set("n", "<C-e>", "<C-e>zz", { remap = true, desc = "Go up and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Go up and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Go down and center" })
+vim.keymap.set("n", "<C-y>", "<C-y>zz", { desc = "Go down and center" })
+vim.keymap.set("n", "<C-e>", "<C-e>zz", { desc = "Go up and center" })
 
-vim.keymap.set("n", "<BS>", "kdd", { remap = true, desc = "Delete line above" })
-vim.keymap.set("n", "<CR>", "O<Esc>j", { remap = true, desc = "I'm not remember what this's for" })
+vim.keymap.set("n", "<BS>", "kdd", { desc = "Delete line above" })
+vim.keymap.set("n", "<CR>", "O<Esc>j", { desc = "I'm not remember what this's for" })
 
-vim.keymap.set("n", "g*", "g*zz", { remap = true })
-vim.keymap.set("n", "g#", "g#zz", { remap = true })
-vim.keymap.set("n", "gD", "gDzz", { remap = true })
-vim.keymap.set("n", "gd", "gdZZ", { remap = true })
+vim.keymap.set("n", "g*", "g*zz", {})
+vim.keymap.set("n", "g#", "g#zz", {})
+vim.keymap.set("n", "gD", "gDzz", {})
+vim.keymap.set("n", "gd", "gdZZ", {})
 
 vim.keymap.set("x", "<Tab>", ">gv", { remap = true })
 vim.keymap.set("x", "<S-Tab>", "<gv", { remap = true })
 
-vim.keymap.set("n", "Y", '"*y', { remap = true })
-vim.keymap.set("n", "P", '"*p', { remap = true })
+vim.keymap.set({ "n", "v" }, "Y", '"*y', { remap = true })
+vim.keymap.set({ "n", "v" }, "P", '"*p', { remap = true })
