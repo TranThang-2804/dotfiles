bccabf9056dd3f2625e28e63d61576c84b83e374
Tran Thang <tranthang.dev@gmail.com>
2023-11-03 13:45:19 +0700

    configured toggle_term keybinding

--- LazyVim/lua/plugins/toggle_term.lua
+++ LazyVim/lua/plugins/toggle_term.lua
@@ -1,6 +1,18 @@
 return {
   -- amongst your other plugins
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    keys = {
+      { "<leader>th", "<cmd>ToggleTerm size=40 direction=horizontal<cr>", desc = "Open a horizontal terminal" },
+      { "<leader>tv", "<cmd>ToggleTerm size=40 direction=vertical<cr>", desc = "Open a vertical terminal" },
+
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
+      { "<C-_>", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
+      { "<C-/>", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
+    },
+  },
   -- or
   -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
 }
