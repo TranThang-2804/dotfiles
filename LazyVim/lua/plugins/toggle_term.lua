3575d0363cc99c8c5fabc0f9acd3b162895124d0
Tran Thang <tranthang.dev@gmail.com>
2023-11-03 15:34:09 +0700

    update

--- LazyVim/lua/plugins/toggle_term.lua
+++ LazyVim/lua/plugins/toggle_term.lua
@@ -8,10 +8,12 @@ return {
     version = "*",
     config = true,
     keys = {
-      { "th", "<C-h>", desc = "Open a horizontal terminal" },
-      { "tv", "<C-v>", desc = "Open a vertical terminal" },
-      { "<C-h>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Open a horizontal terminal" },
-      { "<C-v>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Open a vertical terminal" },
+      {
+        "<leader>th",
+        '<cmd>exe v:count1 . "ToggleTerm" direction=horizontal<CR>',
+        desc = "Open a horizontal terminal",
+      },
+      { "<leader>tv", '<cmd>exe v:count1 . "ToggleTerm" direction=vertical<CR>', desc = "Open a vertical terminal" },
 
       { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
       { "<C-_>", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
