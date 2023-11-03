d0ac14610a8a7e62f0b62e91811ccc5a4cfdf70c
Tran Thang <tranthang.dev@gmail.com>
2023-11-03 15:05:59 +0700

    update

--- LazyVim/lua/plugins/toggle_term.lua
+++ LazyVim/lua/plugins/toggle_term.lua
@@ -1,3 +1,6 @@
+local function openFloatTerminal(number, direction)
+  vim.cmd(":" .. number .. "ToggleTerm direction=")
+end
 return {
   -- amongst your other plugins
   {
@@ -5,14 +8,11 @@ return {
     version = "*",
     config = true,
     keys = {
-      { "<leader>th", "<cmd>ToggleTerm size=40 direction=horizontal<cr>", desc = "Open a horizontal terminal" },
-      { "<leader>tv", "<cmd>ToggleTerm size=40 direction=vertical<cr>", desc = "Open a vertical terminal" },
+      { "th", "<cmd>ToggleTerm size=60 direction=horizontal<cr>", desc = "Open a horizontal terminal" },
+      { "tv", "<cmd>ToggleTerm size=40 direction=vertical<cr>", desc = "Open a vertical terminal" },
 
       { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
       { "<C-_>", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
-      { "<C-/>", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" },
     },
   },
-  -- or
-  -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
 }
