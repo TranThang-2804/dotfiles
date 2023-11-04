222fa73e26a7fd99c71f6c743e6a63fa543b69c6
Tran Thang <tranthang.dev@gmail.com>
2023-11-04 16:20:11 +0700

    removed cmake pluggin

--- neovim_configuration/lua/config/lazy.lua
+++ neovim_configuration/lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.go" },
     -- import any extras modules here
