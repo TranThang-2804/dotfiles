efa4dd63f912f9656fdfc734316709fdb11af260
Tran Thang <tranthang.dev@gmail.com>
2023-11-04 15:32:16 +0700

    update

--- neovim_configuration/lua/config/lazy.lua
+++ neovim_configuration/lua/config/lazy.lua
@@ -10,6 +10,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
---
+++ neovim_configuration/lua/plugins/mini_comment.lua
@@ -0,0 +1,17 @@
+-- return {
+--   { "echasnovski/mini.nvim", version = false },
+--   -- Options which control module behavior
+--   options = {
+--     -- Function to compute custom 'commentstring' (optional)
+--     custom_commentstring = nil,
+--
+--     -- Whether to ignore blank lines
+--     ignore_blank_line = false,
+--
+--     -- Whether to recognize as comment only lines without indent
+--     start_of_line = false,
+--
+--     -- Whether to ensure single space pad for comment parts
+--     pad_comment_parts = true,
+--   },
+-- }
