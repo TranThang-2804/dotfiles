fb9750487a2aec404f0f332911f3d6d1f30ff04c
Tran Thang <tranthang.dev@gmail.com>
2023-11-05 23:13:56 +0700

    update terminal esc key to C-b

--- neovim_configuration/lua/config/keymaps.lua
+++ neovim_configuration/lua/config/keymaps.lua
@@ -46,4 +46,4 @@ vim.keymap.set("n", "˙", "<cmd>vertical resize -2<cr>", { desc = "Decrease wind
 vim.keymap.set("n", "¬", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- This is for global config for terminal mode
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "esc to navigate in terminal mode" })
+vim.keymap.set("t", "<C-b>", "<C-\\><C-n>", { desc = "esc outside of insert mode in terminal to navigate" })
