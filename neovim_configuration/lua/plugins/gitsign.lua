f988fe62817131c66a155b8214b43fc39d87d218
Tran Thang <tranthang.dev@gmail.com>
2023-11-05 17:32:26 +0700

    added git blame into the UI

---
+++ neovim_configuration/lua/plugins/gitsign.lua
@@ -0,0 +1,39 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "LazyFile",
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    attach_to_untracked = true,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+      delay = 0,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
+    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil, -- Use default
+    max_file_length = 40000, -- Disable if file is longer than this (in lines)
+    preview_config = {
+      -- Options passed to nvim_open_win
+      border = "single",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
+    yadm = {
+      enable = false,
+    },
+  },
+}
