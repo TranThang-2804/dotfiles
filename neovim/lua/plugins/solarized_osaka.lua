f2210e7873224705243254f82e19f7621a6e88f4
Tran Thang <tranthang.dev@gmail.com>
2023-11-07 19:13:10 +0700

    update theme

--- neovim/lua/plugins/solarized_osaka.lua
+++ neovim/lua/plugins/solarized_osaka.lua
@@ -11,17 +11,22 @@ return {
       -- Style to be applied to different syntax groups
       -- Value is any valid attr-list value for `:help nvim_set_hl`
       comments = { italic = true },
-      keywords = { italic = true },
-      functions = {},
+      keywords = { bold = true },
+      conditionals = { italic = true },
+      loops = { italic = true },
+      functions = { italic = true },
+      strings = {},
       variables = {},
+      numbers = { "italic" },
+      booleans = { "italic" },
       -- Background styles. Can be "dark", "transparent" or "normal"
-      sidebars = "dark", -- style for sidebars, see below
+      sidebars = "transparent", -- style for sidebars, see below
       floats = "dark", -- style for floating windows
     },
-    sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    sidebars = { "qf", "help", "terminal", "floats", "sidebars" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+    day_brightness = 1, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    dim_inactive = true, -- dims inactive windows
+    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
   },
 }
