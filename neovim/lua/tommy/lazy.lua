459627fc3afbc2dd405da0dfe055d4a37f3f99bf
Tran Thang <tranthang.dev@gmail.com>
2023-11-12 02:34:44 +0700

    added minisurround

--- neovim/lua/tommy/lazy.lua
+++ neovim/lua/tommy/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({ 
   { import = "tommy.plugins" },
   { import = "tommy.plugins.themes" },
+  { import = "tommy.plugins.mini" },
   { import = "tommy.plugins.lsp" }
 },
 {
