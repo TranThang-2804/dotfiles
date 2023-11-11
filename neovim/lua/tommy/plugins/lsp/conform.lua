6340140327329cb4af024fc875bbe054efe6a298
Tran Thang <tranthang.dev@gmail.com>
2023-11-12 04:34:07 +0700

    finalize

--- neovim/lua/tommy/plugins/lsp/conform.lua
+++ neovim/lua/tommy/plugins/lsp/conform.lua
@@ -1,16 +1,37 @@
 return {
   "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = true,
-  cmd = "ConformInfo",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
   keys = {
     {
-      "<leader>cF",
+      -- Customize or remove this keymap to your liking
+      "<leader>cf",
       function()
-        require("conform").format({ formatters = { "injected" } })
+        require("conform").format({ async = true, lsp_fallback = true })
       end,
-      mode = { "n", "v" },
-      desc = "Format Injected Langs",
+      mode = "",
+      desc = "Format buffer",
     },
   },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { { "prettierd", "prettier" } },
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
--- neovim/lua/tommy/plugins/toggle_term.lua
+++ neovim/lua/tommy/plugins/toggle_term.lua
@@ -1,24 +1,22 @@
-return 
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-    keys = {
-      {
-        "<leader>th",
-        '<cmd>exe v:count1 . "ToggleTerm direction=horizontal"<CR>',
-       desc = "Open a horizontal terminal",
-      },
-      {
-        "<leader>tv",
-        '<cmd>exe v:count1 . "ToggleTerm direction=vertical size=40"<CR>',
-        desc = "Open a vertical terminal",
-      },
-      {
-        "<C-t>",
-        '<cmd>ToggleTerm direction=float<CR>',
-        desc = "Open a float terminal",
-      },
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = true,
+  keys = {
+    {
+      "<leader>th",
+      '<cmd>exe v:count1 . "ToggleTerm direction=horizontal"<CR>',
+      desc = "Open a horizontal terminal",
     },
-  }
-
+    {
+      "<leader>tv",
+      '<cmd>exe v:count1 . "ToggleTerm direction=vertical size=40"<CR>',
+      desc = "Open a vertical terminal",
+    },
+    {
+      "<C-t>",
+      "<cmd>ToggleTerm direction=float<CR>",
+      desc = "Open a float terminal",
+    },
+  },
+}
