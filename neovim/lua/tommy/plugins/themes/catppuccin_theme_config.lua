5f28d825d6afefb634e1e69dc700dcb44080aada
Tran Thang <tranthang.dev@gmail.com>
2023-11-12 11:37:28 +0700

    update better theme

--- neovim/lua/tommy/plugins/themes/catppuccin_theme_config.lua
+++ neovim/lua/tommy/plugins/themes/catppuccin_theme_config.lua
@@ -10,9 +10,9 @@ return {
     },
     transparent_background = true, -- disables setting the background color.
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
-      enabled = true, -- dims the background color of inactive window
+      enabled = false, -- dims the background color of inactive window
       shade = "dark",
       percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
