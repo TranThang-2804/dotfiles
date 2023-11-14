94fd23e1561652b8ad5b7f93e6b9ae7664e261c9
Tran Thang <tranthang.dev@gmail.com>
2023-11-15 01:10:21 +0700

    fixed glitch in bufferline

--- neovim/lua/tommy/plugins/ui/bufferline.lua
+++ neovim/lua/tommy/plugins/ui/bufferline.lua
@@ -20,14 +20,6 @@ return {
       right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
       diagnostics = "nvim_lsp",
       always_show_bufferline = false,
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "Neo-tree",
-          highlight = "Directory",
-          text_align = "left",
-        },
-      },
     },
     highlights = {
       buffer_selected = {
@@ -38,15 +30,4 @@ return {
       }
     }
   },
-  config = function(_, opts)
-    require("bufferline").setup(opts)
-    -- Fix bufferline when restoring a session
-    vim.api.nvim_create_autocmd("BufAdd", {
-      callback = function()
-        vim.schedule(function()
-          pcall(nvim_bufferline)
-        end)
-      end,
-    })
-  end,
 }
