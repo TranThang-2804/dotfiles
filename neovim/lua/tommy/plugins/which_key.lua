349c682813097195d1df91e962c4f174bd95d936
Tran Thang <tranthang.dev@gmail.com>
2023-11-26 20:50:33 +0700

    update whichkey

--- neovim/lua/tommy/plugins/which_key.lua
+++ neovim/lua/tommy/plugins/which_key.lua
@@ -19,6 +19,7 @@ return {
       ["<leader>f"] = { name = "+file/find" },
       ["<leader>g"] = { name = "+git" },
       ["<leader>gh"] = { name = "+hunks" },
+      ["<leader>l"] = { name = "+leetcode" },
       ["<leader>q"] = { name = "+quit/session" },
       ["<leader>s"] = { name = "+search" },
       ["<leader>u"] = { name = "+ui" },
