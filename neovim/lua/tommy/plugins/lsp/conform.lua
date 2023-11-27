ddce5e2b20dc28a8b1899c8d2150b48be2bf81d9
Tran Thang <tranthang.dev@gmail.com>
2023-11-28 00:55:26 +0700

    added autosave plugin

--- neovim/lua/tommy/plugins/lsp/conform.lua
+++ neovim/lua/tommy/plugins/lsp/conform.lua
@@ -22,7 +22,7 @@ return {
       javascript = { { "prettierd", "prettier" } },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
     -- Customize formatters
     formatters = {
       shfmt = {
