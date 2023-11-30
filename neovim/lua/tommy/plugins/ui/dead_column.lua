d137646ba14a6955f9781070714fcea549757c05
Tran Thang <tranthang.dev@gmail.com>
2023-11-30 13:21:46 +0700

    added dead colume plugin

---
+++ neovim/lua/tommy/plugins/ui/dead_column.lua
@@ -0,0 +1,9 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  opts = {
+    colorcolumn = "80",
+    disabled_filetypes = { "help", "text", "markdown" },
+    custom_colorcolumn = {},
+    scope = "file",
+  }
+}
