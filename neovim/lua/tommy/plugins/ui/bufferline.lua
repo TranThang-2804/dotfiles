b7493ea847f294395d993467aecc55700ec08068
Tran Thang <tranthang.dev@gmail.com>
2023-11-30 12:42:25 +0700

    update bufferline

--- neovim/lua/tommy/plugins/ui/bufferline.lua
+++ neovim/lua/tommy/plugins/ui/bufferline.lua
@@ -7,6 +7,7 @@ return {
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>",          desc = "Delete other buffers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",           desc = "Delete buffers to the right" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",            desc = "Delete buffers to the left" },
+    { "<leader>bc", "<Cmd>BufferLinePick<CR>",                 desc = "Delete buffers to the left" },
     { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev buffer" },
     { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",            desc = "Next buffer" },
     { "[b",         "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev buffer" },
@@ -14,11 +15,12 @@ return {
   },
   opts = {
     options = {
+      mode = "buffers",
       -- stylua: ignore
       close_command = function(n) require("mini.bufremove").delete(n, false) end,
       -- stylua: ignore
       right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-      diagnostics = "nvim_lsp",
+      -- diagnostics = "nvim_lsp",
       always_show_bufferline = false,
     },
     highlights = {
