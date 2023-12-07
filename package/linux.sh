a12bfb3dc0c27fcd7265a52ac8f6fd388e1e52c6
TommyZ <tranthang.dev@gmail.com>
2023-12-07 13:55:07 +0700

    update:

--- package/linux.sh
+++ package/linux.sh
@@ -8,10 +8,10 @@ sudo apt install curl git gcc -y
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-install lazygit /usr/local/bin
+sudo install lazygit /usr/local/bin
 
 # Install nerd-fonts
-mkdir -p ~/.local/share/fonts
+sudo mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 ./install.sh
 
