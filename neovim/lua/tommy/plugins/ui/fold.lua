ab3d3418734cef1bba291665a21fc624df77b077
TommyZ <tranthang.dev@gmail.com>
2023-12-07 13:52:13 +0700

    update

--- neovim/lua/tommy/plugins/ui/fold.lua
+++ neovim/lua/tommy/plugins/ui/fold.lua
@@ -1,54 +1,54 @@
 return {
-  'kevinhwang91/nvim-ufo',
-  dependancies = {
-    'kevinhwang91/promise-async'
-  },
-  config = function()
-    -- vim.o.foldcolumn = '1' -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
-    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
-
-
-    local handler = function(virtText, lnum, endLnum, width, truncate)
-      local newVirtText = {}
-      local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-      local sufWidth = vim.fn.strdisplaywidth(suffix)
-      local targetWidth = width - sufWidth
-      local curWidth = 0
-      for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-          table.insert(newVirtText, chunk)
-        else
-          chunkText = truncate(chunkText, targetWidth - curWidth)
-          local hlGroup = chunk[2]
-          table.insert(newVirtText, { chunkText, hlGroup })
-          chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          -- str width returned from truncate() may less than 2nd argument, need padding
-          if curWidth + chunkWidth < targetWidth then
-            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-          end
-          break
-        end
-        curWidth = curWidth + chunkWidth
-      end
-      table.insert(newVirtText, { suffix, 'MoreMsg' })
-      return newVirtText
-    end
-
-    require('ufo').setup({
-      fold_virt_text_handler = handler,
-      provider_selector = function(bufnr, filetype, buftype)
-        return { 'indent' }
-      end
-    })
-  end,
+--   'kevinhwang91/nvim-ufo',
+--   dependancies = {
+--     'kevinhwang91/promise-async'
+--   },
+--   config = function()
+--     -- vim.o.foldcolumn = '1' -- '0' is not bad
+--     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+--     vim.o.foldlevelstart = 99
+--     vim.o.foldenable = true
+--
+--     -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+--     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+--     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+--     vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+--     vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+--
+--
+--     local handler = function(virtText, lnum, endLnum, width, truncate)
+--       local newVirtText = {}
+--       local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+--       local sufWidth = vim.fn.strdisplaywidth(suffix)
+--       local targetWidth = width - sufWidth
+--       local curWidth = 0
+--       for _, chunk in ipairs(virtText) do
+--         local chunkText = chunk[1]
+--         local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+--         if targetWidth > curWidth + chunkWidth then
+--           table.insert(newVirtText, chunk)
+--         else
+--           chunkText = truncate(chunkText, targetWidth - curWidth)
+--           local hlGroup = chunk[2]
+--           table.insert(newVirtText, { chunkText, hlGroup })
+--           chunkWidth = vim.fn.strdisplaywidth(chunkText)
+--           -- str width returned from truncate() may less than 2nd argument, need padding
+--           if curWidth + chunkWidth < targetWidth then
+--             suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+--           end
+--           break
+--         end
+--         curWidth = curWidth + chunkWidth
+--       end
+--       table.insert(newVirtText, { suffix, 'MoreMsg' })
+--       return newVirtText
+--     end
+--
+--     require('ufo').setup({
+--       fold_virt_text_handler = handler,
+--       provider_selector = function(bufnr, filetype, buftype)
+--         return { 'indent' }
+--       end
+--     })
+--   end,
 }
--- neovim/lua/tommy/plugins/ui/fold_promise.lua
+++ neovim/lua/tommy/plugins/ui/fold_promise.lua
@@ -1,3 +1,3 @@
 return {
-  'kevinhwang91/promise-async'
+  -- 'kevinhwang91/promise-async'
 }
