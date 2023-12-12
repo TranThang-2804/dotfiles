14cb6659cae6e9b3c9b4c246da29f33962101ba9
TommyZ <tranthang.dev@gmail.com>
2023-12-12 15:02:42 +0700

    disable smooth scrolling

--- neovim/lua/tommy/plugins/ui/smooth_scrolling.lua
+++ neovim/lua/tommy/plugins/ui/smooth_scrolling.lua
@@ -1,10 +1,10 @@
 return {
-  'declancm/cinnamon.nvim',
-  config = function()
-    require('cinnamon').setup({
-      override_keymaps = true,
-      max_length = 500,
-      scroll_limit = -1,
-    })
-  end
+  -- 'declancm/cinnamon.nvim',
+  -- config = function()
+  --   require('cinnamon').setup({
+  --     override_keymaps = true,
+  --     max_length = 500,
+  --     scroll_limit = -1,
+  --   })
+  -- end
 }
