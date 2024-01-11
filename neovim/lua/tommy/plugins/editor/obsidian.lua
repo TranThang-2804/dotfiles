0d9ec18f6946a63e7bb84a3aad879e5da0384f23
Tran Thang <tranthang.dev@gmail.com>
2024-01-11 14:23:24 +0700

    update obsidian mapping

--- neovim/lua/tommy/plugins/editor/obsidian.lua
+++ neovim/lua/tommy/plugins/editor/obsidian.lua
@@ -1,8 +1,8 @@
 return {
   "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
+  ft = "*",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -21,6 +21,72 @@ return {
       {
         name = "tommy-note-vault",
         path = "/Users/tranthang/Library/Mobile Documents/iCloud~md~obsidian/Documents/tommy-note-vault",
+      },
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%d-%m-%Y",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil
+    },
+    mappings = {
+      ["<leader>nf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true, desc = "passthrough" },
+      },
+      ["<leader>nc"] = {
+        action = function()
+          return "<cmd>ObsidianQuickSwitch<CR>"
+        end,
+        opts = { noremap = false, expr = true, buffer = true, desc = "quick switch" },
+      },
+      ["<leader>ncb"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true, desc = "toggle checkbox" },
+      },
+      ["<leader>ntd"] = {
+        action = function()
+          return "<cmd>ObsidianToday<CR>"
+        end,
+        opts = { buffer = true, desc = "today note" },
+      },
+      ["<leader>ntm"] = {
+        action = function()
+          return "<cmd>ObsidianTomorrow<CR>"
+        end,
+        opts = { buffer = true, desc = "tomorrow note" },
+      },
+      ["<leader>ny"] = {
+        action = function()
+          return "<cmd>ObsidianYesterday<CR>"
+        end,
+        opts = { buffer = true, desc = "yesterday note" },
+      },
+      ["<leader>nn"] = {
+        action = function()
+          return "<cmd>ObsidianNew<CR>"
+        end,
+        opts = { buffer = true, desc = "new note" },
+      },
+      ["<leader>ns"] = {
+        action = function()
+          return "<cmd>ObsidianSearch<CR>"
+        end,
+        opts = { buffer = true, desc = "search" },
+      },
+      ["<leader>np"] = {
+        action = function()
+          return "<cmd>ObsidianPasteImage<CR>"
+        end,
+        opts = { buffer = true, desc = "paste image" },
       },
     },
   },
