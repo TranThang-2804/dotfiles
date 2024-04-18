f8307a5307dc84ac1940f32cc24c22fdaef7ea30
ThangTD18.NGT <tranthang.dev@gmail.com>
2024-04-19 01:11:12 +0700

    fixed obsidiant

--- package/linux.sh
+++ package/linux.sh
@@ -8,6 +8,8 @@ sudo apt install zsh -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+mkdir ~/Documents/Note
+
 # Install Lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
