93263d3334d0ec00b6c68f2bc107e9e71b6c3f3c
ThangTD18.NGT <thangtd18ngt@LPP00248757E.fsoft.fpt.vn>
2024-04-18 16:25:26 +0700

    update

--- package/linux.sh
+++ package/linux.sh
@@ -13,12 +13,13 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm -rf lazygit
+rm -rf lazygit.tar.gz
 
 # Install nerd-fonts
-sudo mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && git clone --depth=1 https://github.com/ryanoasis/nerd-fonts
-cd ~/.local/share/fonts/nerd-fonts
-./install.sh
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts
+sudo ./nerd-fonts/install.sh
+rm -rf nerd-fonts
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -31,3 +32,6 @@ sudo snap install nvim --classic
 sudo apt-get install ripgrep
 
 sudo apt install xclip
+
+# Set zsh default
+sudo chsh -s $(which zsh)
