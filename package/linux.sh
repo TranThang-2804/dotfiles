4cef1a4ceeeebec538b545f8eea607945b6511f4
ThangTD18.NGT <tranthang.dev@gmail.com>
2024-04-19 01:31:29 +0700

    update

--- package/linux.sh
+++ package/linux.sh
@@ -6,9 +6,37 @@ sudo apt install curl git gcc -y
 sudo apt install tmux -y
 sudo apt install zsh -y
 
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+sudo apt-get update -y
+sudo apt-get install kubectl -y
+
+sudo apt install docker docker.io -y
+sudo usermod -aG docker $USER
+newgrp docker
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get install helm -y
+
+sudo apt install nodejs -y
+
+sudo apt install python3 pip -y
+
+sudo apt install awscli -y
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 mkdir ~/Documents/Note
+#
+# Install zsh tools
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 
 # Install Lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -38,3 +66,4 @@ sudo apt install xclip
 
 # Set zsh default
 sudo chsh -s $(which zsh)
+
