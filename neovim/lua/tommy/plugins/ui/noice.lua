6f64db4f0beed8933bc131cec83cd1283ce2b13b
root <tranthang.dev@gmail.com>
2024-05-21 08:39:58 +0000

    gs

--- neovim/lua/tommy/plugins/ui/noice.lua
+++ neovim/lua/tommy/plugins/ui/noice.lua
@@ -23,8 +23,8 @@ return {
       },
     },
     presets = {
-      bottom_search = true,
-      -- command_palette = true,
+      -- bottom_search = true,
+      command_palette = true,
       long_message_to_split = true,
       inc_rename = true,
     },
