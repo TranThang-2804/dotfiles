545f89cd727fd704d78b9933b5c8b4fab6d801b3
ThangTD18.NGT <tranthang.dev@gmail.com>
2024-05-28 14:31:37 +0700

    update

--- neovim/lua/tommy/plugins/themes/solarized_osaka.lua
+++ neovim/lua/tommy/plugins/themes/solarized_osaka.lua
@@ -1,11 +1,12 @@
 return {
   "craftzdog/solarized-osaka.nvim",
+  name = "solarized-osaka",
   lazy = false,
   priority = 1000,
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    transparent = false,    -- Enable this to disable setting the background color
+    transparent = true,    -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
     styles = {
       -- Style to be applied to different syntax groups
