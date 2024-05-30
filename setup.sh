7afb28fb16d5c4f58905d458d217449d2307e978
tran thang <tranthang.dev@gmail.com>
2024-05-30 19:52:37 +0700

    update flake

--- setup.sh
+++ setup.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-WORKING_DIR=$(pwd)
+#WORKING_DIR=$(pwd)
 
-INSTALL_PACKAGE=$1
+#INSTALL_PACKAGE=$1
 
 # Check the operating system
-echo '--------Checking machine OS--------'
-if [[ "$(uname)" == "Darwin" && $INSTALL_PACKAGE == "all" ]]; then
-	# Action for macOS
-	echo "Running on macOS"
-	cd "$WORKING_DIR/package"
-	. ./macos.sh
-	cd "$WORKING_DIR"
-elif [[ "$(uname)" == "Linux" && $INSTALL_PACKAGE == "all" ]]; then
-	# Action for Linux
-	echo "Running on Linux"
-	cd "$WORKING_DIR/package"
-	. ./linux.sh
-	cd "$WORKING_DIR"
-elif [[ $INSTALL_PACKAGE == "docker" ]]; then
-	# Action for Linux
-	echo "Running on Linux"
-	cd "$WORKING_DIR/package"
-	. ./docker.sh
-	cd "$WORKING_DIR"
-fi
+#echo '--------Checking machine OS--------'
+#if [[ "$(uname)" == "Darwin" && $INSTALL_PACKAGE == "all" ]]; then
+#	# Action for macOS
+#	echo "Running on macOS"
+#	cd "$WORKING_DIR/package"
+#	. ./macos.sh
+#	cd "$WORKING_DIR"
+#elif [[ "$(uname)" == "Linux" && $INSTALL_PACKAGE == "all" ]]; then
+#	# Action for Linux
+#	echo "Running on Linux"
+#	cd "$WORKING_DIR/package"
+#	. ./linux.sh
+#	cd "$WORKING_DIR"
+#elif [[ $INSTALL_PACKAGE == "docker" ]]; then
+#	# Action for Linux
+#	echo "Running on Linux"
+#	cd "$WORKING_DIR/package"
+#	. ./docker.sh
+#	cd "$WORKING_DIR"
+#fi
 
 # Setup dotfiles
 sudo cp -arf ./src/. ~
 echo '--------Set up dotfiles configuration finished-------'
+
+# Run flake
+home-manager switch --flake src/.config/home-manager#tommy                   
