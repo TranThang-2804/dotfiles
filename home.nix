ab10a67c409e5b79960064837cc0454b537061dc
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2024-06-04 13:22:14 +0700

    update

--- home.nix
+++ home.nix
@@ -137,7 +137,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.05";
+  home.stateVersion = "24.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
