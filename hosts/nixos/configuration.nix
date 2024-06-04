f114feca924729dce276ed5002546f44d279641d
Tran Thang <tranthang.dev@gmail.com>
2024-06-04 22:14:02 +0700

    revert changes to previous

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -124,11 +124,11 @@
   
   security.polkit.enable = true;
 
-  programs.i3.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_RENDERER = "vulkan";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
-  environment.sessionVariables.XWAYLAND_NO_GLAMOR = "1";
+  #windowManager.i3.enable = true;
+  #environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  #environment.sessionVariables.WLR_RENDERER = "vulkan";
+  #environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  #environment.sessionVariables.XWAYLAND_NO_GLAMOR = "1";
   
   ## Install Hyprland
   #programs.hyprland.enable = true;
@@ -200,31 +200,5 @@
     (nerdfonts.override { fonts = [ "JetBrainsMono"]; })
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
