286956d921e2a73efd9f3ca790825166ddf34ee4
Tran Thang <tranthang.dev@gmail>
2024-07-12 23:04:42 +0700

    add lazygit to toggleterm

--- dotfiles/.config/nvim/lua/tommy/plugins/editor/toggle_term.lua
+++ dotfiles/.config/nvim/lua/tommy/plugins/editor/toggle_term.lua
@@ -1,7 +1,6 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
   keys = {
     {
       "<leader>th",
@@ -21,9 +20,38 @@ return {
     },
     {
       "<C-t>",
-      "<cmd>68ToggleTerm direction=float<CR>",
+      "<cmd>68ToggleTerm direction=float name=float-terminal<CR>",
       desc = "Open a float terminal",
       mode = { "n", "x", "o", "i", "t", "s" },
     },
   },
+  config = function()
+    require("toggleterm").setup()
+    local Terminal = require("toggleterm.terminal").Terminal
+
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      count = 69,
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      -- function to run on opening the terminal
+      -- on_open = function(term)
+      --   vim.cmd("startinsert!")
+      --   vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      -- end,
+      -- -- function to run on closing the terminal
+      -- on_close = function(term)
+      --   vim.cmd("startinsert!")
+      -- end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+  end
 }
