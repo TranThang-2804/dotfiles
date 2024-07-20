457f678d0fe281a84f9388725fc53446c7d70f1b
Tran Thang <tranthang.dev@gmail>
2024-07-21 00:27:17 +0700

    format code

--- dotfiles/.config/nvim/lua/tommy/plugins/lsp/mason.lua
+++ dotfiles/.config/nvim/lua/tommy/plugins/lsp/mason.lua
@@ -1,8 +1,8 @@
 return {
   "williamboman/mason.nvim",
   cmd = "Mason",
-  keys = { 
-    { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } 
+  keys = {
+    { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" }
   },
   config = function()
     local mason = require("mason")
