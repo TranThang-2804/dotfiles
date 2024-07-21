6e31cd4e64b9a3e8118df9863fd4f0c191eba24d
Tran Thang <tranthang.dev@gmail>
2024-07-21 12:49:01 +0700

    Update keymap

--- dotfiles/.config/nvim/lua/tommy/core/keymaps.lua
+++ dotfiles/.config/nvim/lua/tommy/core/keymaps.lua
@@ -14,18 +14,18 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = tr
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
-vim.keymap.set("n", "<C-r>k", "<cmd>resize +10<cr>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-r>j", "<cmd>resize -10<cr>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-r>h", "<cmd>vertical resize -10<cr>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-r>l", "<cmd>vertical resize +10<cr>", { desc = "Increase window width" })
+vim.keymap.set("n", "<C-r>k", "<cmd>resize +20<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-r>j", "<cmd>resize -20<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-r>h", "<cmd>vertical resize -20<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-r>l", "<cmd>vertical resize +20<cr>", { desc = "Increase window width" })
 
 -- Move Lines
 vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
 vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
 vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Clear search with <esc>
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
