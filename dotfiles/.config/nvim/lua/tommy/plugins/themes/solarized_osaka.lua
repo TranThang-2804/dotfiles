7b7c776aa58a6c9883bbe8b3483b2db0d59d7904
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2024-10-15 15:26:50 +0700

    adding new theme

--- dotfiles/.config/nvim/lua/tommy/plugins/themes/solarized_osaka.lua
+++ dotfiles/.config/nvim/lua/tommy/plugins/themes/solarized_osaka.lua
@@ -6,7 +6,7 @@ return {
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    transparent = true,    -- Enable this to disable setting the background color
+    transparent = false,    -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
     styles = {
       -- Style to be applied to different syntax groups
