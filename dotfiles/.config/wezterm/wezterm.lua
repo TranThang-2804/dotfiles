8e19917e00801e5bf0b1291805b577803a7a86dd
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2024-10-28 19:45:57 +0900

    feat: remove transparent theme

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -36,7 +36,7 @@ local config = {
   enable_tab_bar = true,
 
   window_decorations = "RESIZE",
-  window_background_opacity = 0.8,
+  window_background_opacity = 1,
   text_background_opacity = 1.0,
   macos_window_background_blur = 10,
 
