5c2ff2aaf6ea155d14ff9f8ca67a9c3e8c33473b
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2024-10-30 21:01:07 +0900

    chore: clean up copilot config

--- dotfiles/.config/nvim/lua/tommy/plugins/cmp/copilot-chat.lua
+++ dotfiles/.config/nvim/lua/tommy/plugins/cmp/copilot-chat.lua
@@ -136,48 +136,24 @@ return {
         end,
       })
 
-      -- Add which-key mappings
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>ac",  group = "+Copilot action" }, -- group
-        { "<leader>acd", desc = "Show diff" },
-        { "<leader>acp", desc = "System prompt" },
-        { "<leader>acs", desc = "Show selection" },
-        { "<leader>acy", desc = "Yank diff" },
-      })
+      -- -- Add which-key mappings
+      -- local wk = require("which-key")
+      -- wk.add({
+      --   { "<leader>ac",  group = "+Copilot action" }, -- group
+      --   { "<leader>acd", desc = "Show diff" },
+      --   { "<leader>acp", desc = "System prompt" },
+      --   { "<leader>acs", desc = "Show selection" },
+      --   { "<leader>acy", desc = "Yank diff" },
+      -- })
     end,
     event = "VeryLazy",
     keys = {
-      -- Show help actions with telescope
-      {
-        "<leader>ah",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-      },
-      -- Show prompts actions with telescope
-      {
-        "<leader>ap",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
       {
         "<leader>ap",
         ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
         mode = "x",
         desc = "CopilotChat - Prompt actions",
       },
-      -- Code related commands
-      { "<leader>ae", "<cmd>CopilotChatExplain<cr>",       desc = "CopilotChat - Explain code" },
-      { "<leader>aT", "<cmd>CopilotChatTests<cr>",         desc = "CopilotChat - Generate tests" },
-      { "<leader>ar", "<cmd>CopilotChatReview<cr>",        desc = "CopilotChat - Review code" },
-      { "<leader>aR", "<cmd>CopilotChatRefactor<cr>",      desc = "CopilotChat - Refactor code" },
-      { "<leader>an", "<cmd>CopilotChatBetterNamings<cr>", desc = "CopilotChat - Better Naming" },
       -- Chat with Copilot in visual mode
       {
         "<leader>av",
@@ -185,25 +161,6 @@ return {
         mode = "x",
         desc = "CopilotChat - Open in vertical split",
       },
-      {
-        "<leader>ax",
-        ":CopilotChatInline<cr>",
-        mode = "x",
-        desc = "CopilotChat - Inline chat",
-      },
-      -- Custom input for CopilotChat
-      {
-        "<leader>ai",
-        function()
-          local input = vim.fn.input("Ask Copilot: ")
-          if input ~= "" then
-            vim.cmd("CopilotChat " .. input)
-          end
-        end,
-        desc = "CopilotChat - Ask input",
-      },
-      { "<leader>am", "<cmd>CopilotChatCommit<cr>",       desc = "CopilotChat - Generate commit message for all changes" },
-      { "<leader>aM", "<cmd>CopilotChatCommitStaged<cr>", desc = "CopilotChat - Generate commit message for staged changes" },
       {
         "<leader>aq",
         function()
@@ -214,16 +171,7 @@ return {
         end,
         desc = "CopilotChat - Quick chat",
       },
-      -- Debug
-      { "<leader>ad", "<cmd>CopilotChatDebugInfo<cr>",     desc = "CopilotChat - Debug Info" },
-      -- Fix the issue with diagnostic
-      { "<leader>af", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
-      -- Clear buffer and chat history
-      { "<leader>al", "<cmd>CopilotChatReset<cr>",         desc = "CopilotChat - Clear buffer and chat history" },
-      -- Toggle Copilot Chat Vsplit
-      { "<leader>at", "<cmd>CopilotChatToggle<cr>",        desc = "CopilotChat - Toggle" },
-      -- Copilot Chat Models
-      { "<leader>a?", "<cmd>CopilotChatModels<cr>",        desc = "CopilotChat - Select Models" },
+      { "<leader>at", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
     },
   },
 }
--- dotfiles/.config/nvim/lua/tommy/plugins/cmp/copilot.lua
+++ dotfiles/.config/nvim/lua/tommy/plugins/cmp/copilot.lua
@@ -44,4 +44,7 @@ return {
       server_opts_overrides = {},
     }
   },
+  config = function(_, opts)
+    require("copilot").setup({opts})
+  end,
 }
