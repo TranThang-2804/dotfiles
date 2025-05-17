5e6b248101060b06db5a825d90f5aacc0ff17c5d
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2025-05-18 00:30:21 +0900

    feat: update lsp config to use fzflua

--- config/nvim/lua/tommy/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/tommy/plugins/lsp/lspconfig.lua
@@ -2,8 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   event = { 'BufEnter', 'BufNewFile' },
   config = function()
-    -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
@@ -46,7 +44,7 @@ return {
 
         -- General LSP-related keymaps
         opts.desc = "Show LSP references"
-        keymap.set("n", "<leader>cR", "<cmd>Telescope lsp_references<CR>", opts)
+        keymap.set("n", "<leader>cR", "<cmd>FzfLua lsp_references<CR>", opts)
 
         opts.desc = "Show documentation for what is under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -55,23 +53,26 @@ return {
         keymap.set("n", "<leader>crs", ":LspRestart<CR>", opts)
 
         opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>cD", "<cmd>Telescope diagnostics bufnr=1<CR>", opts)
+        keymap.set("n", "<leader>cD", "<cmd>FzfLua diagnostics_document<CR>", opts)
 
         opts.desc = "Show line diagnostics"
         keymap.set("n", "<leader>cd", vim.diagnostic.open_float, opts)
 
         opts.desc = "Show dynamic workspace symbols"
-        keymap.set("n", "<leader>css", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
+        keymap.set("n", "<leader>css", "<cmd>FzfLua lsp_workspace_symbols<CR>", opts)
 
         -- Per-capability mappings
         if checkBufferMethod('textDocument/declaration') then
           opts.desc = "Go to declaration"
           keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        else
+          opts.desc = "Declaration not supported"
+          keymap.set("n", "gD", function() print("LSP: declaration not supported") end, opts)
         end
 
         if checkBufferMethod('textDocument/definition') then
           opts.desc = "Show LSP definitions"
-          keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>zz", opts)
+          keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", opts)
         else
           opts.desc = "Definition not supported"
           keymap.set("n", "gd", function() print("LSP: definition not supported") end, opts)
@@ -79,35 +80,35 @@ return {
 
         if checkBufferMethod('textDocument/implementation') then
           opts.desc = "Show LSP implementations"
-          keymap.set("n", "<leader>ci", "<cmd>Telescope lsp_implementations<CR>", opts)
+          keymap.set("n", "<leader>ci", "<cmd>FzfLua lsp_implementations<CR>", opts)
+        else
+          opts.desc = "Implementation not supported"
+          keymap.set("n", "<leader>ci", function() print("LSP: implementation not supported") end, opts)
         end
 
         if checkBufferMethod('textDocument/typeDefinition') then
           opts.desc = "Show LSP type definitions"
-          keymap.set("n", "<leader>ct", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+          keymap.set("n", "<leader>ct", "<cmd>FzfLua lsp_typedefs<CR>", opts)
+        else
+          opts.desc = "Type definition not supported"
+          keymap.set("n", "<leader>ct", function() print("LSP: type definition not supported") end, opts)
         end
 
         if checkBufferMethod('textDocument/codeAction') then
           opts.desc = "See available code actions"
           keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        else
+          opts.desc = "Code action not supported"
+          keymap.set({ "n", "v" }, "<leader>ca", function() print("LSP: code action not supported") end, opts)
         end
 
         if checkBufferMethod('textDocument/rename') then
           opts.desc = "Smart rename"
           keymap.set("n", "<leader>crn", vim.lsp.buf.rename, opts)
+        else
+          opts.desc = "Rename not supported"
+          keymap.set("n", "<leader>crn", function() print("LSP: rename not supported") end, opts)
         end
-
-        -- Optional: format on save, if no willSaveWaitUntil but formatting supported
-        -- if not client.supports_method('textDocument/willSaveWaitUntil')
-        --     and client.supports_method('textDocument/formatting') then
-        --   vim.api.nvim_create_autocmd('BufWritePre', {
-        --     group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-        --     buffer = args.buf,
-        --     callback = function()
-        --       vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        --     end,
-        --   })
-        -- end
       end,
     })
 
@@ -139,13 +140,5 @@ return {
         cmd = { 'jdtls' },
         -- root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
       })
-
-    -- configure dartls server (with special settings)
-    -- require("flutter-tools").setup({
-    --   lsp = {
-    --     capabilities = capabilities,
-    --     on_attach = on_attach,
-    --   }
-    -- })
   end,
 }
